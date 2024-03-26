d72a883cbf260cb1754afce4e425945c52bd9385
WonglaRussia <40260598+WonglaRussia@users.noreply.github.com>
2024-03-26 17:28:57 +0300

    working with files using syscalls

--- file.c
+++ file.c
@@ -2,69 +2,50 @@
 #include <stdio.h>			//fputc() fgetc()
 #include <stdlib.h>			//exit()
 #include <errno.h>			//perror()
-#include <sys/stat.h>
+#include <unistd.h>			//open() close() (file)
+#include <fcntl.h>			//file options O_RDONLY e.t.c.
+
 #include "mapping.h"		//define MAP_ROWS
-
-
-int open_file(const char *name) {
-	int descriptor;
-	
-	umask(0002);
-	descriptor = open(name, O_RDWR | O_CREAT | O_APPEND, 0666);
-	if (descriptor == -1 ) {
-		perror("file");
-		exit(1);
-	}
-	else
-		return descriptor;
-}
+#define SIZE_OF_A_MAP 2500
 
 int append_map_to_the_file(int current_map[][MAP_ROWS], char *file_name)
 {
 	int file_descriptor;
+	int len = SIZE_OF_A_MAP;
 	
-	file_descriptor = open_file(const char file_name);
-	write(file_descriptor,current_map, sizeof(current_map));
+	file_descriptor = open(file_name, O_WRONLY | O_CREAT | O_APPEND, 0777);
+	if (file_descriptor == -1 ) {
+		close(file_descriptor);
+		return -1;
+	}
+	else if(len != write(file_descriptor, current_map, SIZE_OF_A_MAP)){
+		perror("while writing to the file");
+		close(file_descriptor);
+		return -1;
+	}
 	close(file_descriptor);
 	return 0;
-	/* 
-	FILE *f;
-	f = fopen(file_name, "ab");
-	if(!f){
-		perror("file");
-		exit(1);
-	}
-	int y, x;
-	for(y = 0; y < MAP_ROWS; y++){
-	  for(x=0; x < MAP_ROWS; x++){
-		fputc(current_map[y][x] , f);
-	  }
-	}
-	fflush(f);
-	fclose(f);
-	return 0;
-	*/
-	
 }
 
-int extract_map(int current_map[][MAP_ROWS], char *file_name, const int round_number)
+//Round number offsets reading map by map. The first round is 0
+int load_map(int current_map[][MAP_ROWS], char *file_name, const int round_number)
 {
-	FILE *f;
-	f = fopen(file_name, "rb");
-	if(!f){
-		perror("file");		//REWRITE filename to global
-		exit(1);
-	}
+	int file_descriptor;
+	int len = SIZE_OF_A_MAP;
 	
-	int y, x;
-	for(y = 0; y < MAP_ROWS; y++){
-	  for(x = 0; x < MAP_ROWS; x++){
-		if(current_map[y][x] == EOF)		//there is no another map in the file
-		  return 100;
-		current_map[y][x] = 0;
-		current_map[y][x] = fgetc(f);
-	  }
+	file_descriptor = open(file_name, O_RDONLY);
+	if (file_descriptor == -1 ) {
+		perror("while open file for reading");
+		return -1;
 	}
-	fclose(f);
+	else {
+		lseek(file_descriptor, SIZE_OF_A_MAP * round_number, SEEK_SET);
+		if (len != read(file_descriptor, current_map, SIZE_OF_A_MAP)){
+			perror("Error while reading the file\n");
+			close(file_descriptor);
+			return -1;
+		}	
+	}
+	close(file_descriptor);
 	return 0;
 }
--- rolling-the-rocks.c
+++ rolling-the-rocks.c
@@ -1,6 +1,7 @@
 /* rolling-the-rocks.c */
 #include <string.h>						//to set string address
 #include <ncurses.h>
+#include <stdlib.h>
 
 #include "maps.h"						//current_map
 #include "rocks.h"						//play_the_map()
@@ -13,23 +14,28 @@ char work_bw;							//b&w terminal?
 
 
 int main() {
-//WRITE writing the map to file
-//WRITE reading the map from the file
-	int round;							//to calculate offcet if the file while reading the map
+	int round;							//to calculate offset in loading the map
 	char filename_with_maps[] = FILE_N_MAPS;					//set the filename [1, infinity]
 	int current_map[MAP_ROWS][MAP_ROWS];
-	
-	score = 40;
-	
-    append_map_to_the_file(old_current_map, filename_with_maps); 	//append the map to the file
+
+    /*
+	if (append_map_to_the_file(old_current_map, filename_with_maps) != 0) 	//append the map to the file
+		return 1;
+	*/
 	
 	initscr();
 	work_bw = !has_colors();						//REWRITE all screen functions
 	keypad(stdscr, 1); 								//esc seq for moving by arrows
 	curs_set(0);
 	
-	for(round = 1; extract_map(current_map, filename_with_maps, round) ; round++)
+	for(round = 0; ; round++)
 	{
+		if ( round == 0)
+			score = 40;
+		if ( load_map(current_map, filename_with_maps, round) != 0 ) {
+			endwin();
+			return 1;
+		}
 		play_the_map(current_map);
 	}
 	endwin();
